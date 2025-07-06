1a0527c2abc8390bc2b4b759cc8d83ac393908ec
Aniket Sundriyal <sundriyalaniket@gmail.com>
2025-07-06 14:45:46 +0530

    Update Loop.java

--- DSA-Java-Basics-to-Advanced by aapna collage/Loop.java
+++ DSA-Java-Basics-to-Advanced by aapna collage/Loop.java
@@ -1,6 +1,6 @@
 public class Loop {
     public static void main (String[]args) {
-        for (int counter =0; counter < 100 ; counter=counter+1){
+        for (int counter =0; counter < 100 ; counter++){
             System.out.println("Hello World");
         }
     }
