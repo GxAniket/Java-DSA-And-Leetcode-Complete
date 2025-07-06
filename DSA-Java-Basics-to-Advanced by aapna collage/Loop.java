8109ce25645584dba068154a6f3d1c2b33b84957
Aniket Sundriyal <sundriyalaniket@gmail.com>
2025-07-06 14:58:41 +0530

    Update Loop.java

--- DSA-Java-Basics-to-Advanced by aapna collage/Loop.java
+++ DSA-Java-Basics-to-Advanced by aapna collage/Loop.java
@@ -1,7 +1,30 @@
+//question 1 
+//WAP TO PRINT HELLO WORLD 100 TIMES USING LOOP
+public class Loop {
+    public static void main (String[]args) {
+        //counter++ => counter=counter+1
+        for (int counter =0; counter < 100 ; counter++){
+            System.out.println("Hello World");
+        }
+    }
+}
+
+//question 2
+//WAP TO PRINT NUMBER TO 100 USING LOOP
 public class Loop {
     public static void main (String[]args) {
         for (int counter =0; counter < 100 ; counter++){
-            System.out.println("Hello World");
+            System.out.println(counter);
+        }
+    }
+}
+
+//question 
+//using i for short
+public class Loop {
+    public static void main (String[]args) {
+        for (int i=0; i< 100 ; i++){
+            System.out.println(i);
         }
     }
 }
