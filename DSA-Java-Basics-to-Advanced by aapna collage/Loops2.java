3723132b7719b7ff3498ab537491543a10023c3b
Aniket Sundriyal <sundriyalaniket@gmail.com>
2025-07-07 09:30:32 +0530

    Add files via upload

---
+++ DSA-Java-Basics-to-Advanced by aapna collage/Loops2.java
@@ -0,0 +1,16 @@
+//wap to print Table 
+import java.util.*;
+
+public class Loops2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= 11; i++) {
+        }
+
+        System.out.println(i*n);
+        sc.close();
+    }
+}
