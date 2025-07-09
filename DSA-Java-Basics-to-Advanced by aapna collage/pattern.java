1db7d75a77ffc3eeb3e7b2881b849a02561b78c0
Aniket Sundriyal <sundriyalaniket@gmail.com>
2025-07-09 11:39:59 +0530

    Add files via upload

--- DSA-Java-Basics-to-Advanced by aapna collage/pattern.java
+++ DSA-Java-Basics-to-Advanced by aapna collage/pattern.java
@@ -1,18 +1,15 @@
-import java.util.*;
-
 public class pattern {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter number of rows: ");
-        int n = sc.nextInt();
+        int n = 4;
+        int m = 5;
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print("* ");
+        for (int i = 1; i<=n; i++) {
+            //inner loop
+            for (int j = 1; j <= m; j++) {
+                //inner lopp
+                System.out.print("*");
             }
             System.out.println();
         }
-
-        sc.close();
     }
 }
