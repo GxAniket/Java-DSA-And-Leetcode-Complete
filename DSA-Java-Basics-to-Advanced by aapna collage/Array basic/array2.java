2f599068cf3ae378d0235bde42d089af74ae4691
Aniket Sundriyal <sundriyalaniket@gmail.com>
2025-07-25 21:25:55 +0530

    Create array2.java

---
+++ DSA-Java-Basics-to-Advanced by aapna collage/Array basic/array2.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+public class array2{
+    public static void main(String[]args){
+        int[]marks = new int[5];
+        marks[0] = 97;
+        marks[1] = 58;
+        marks[2] = 98;
+        marks[3] = 68;
+        marks[4] = 88;
+
+        for(int i = 0; i < marks.length; i++) {
+            System.out.println(marks[i]);
+        }
+    }
+}
