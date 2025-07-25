cfdf3a628bb06d7504d030bbb32e846bbdcdfac9
Aniket Sundriyal <sundriyalaniket@gmail.com>
2025-07-25 21:21:20 +0530

    Create array.java

---
+++ DSA-Java-Basics-to-Advanced by aapna collage/Array basic/array.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+public class array{
+    public static void main(String[]args){
+        int[]marks = new int[5];
+        marks[0] = 97;
+        marks[1] = 58;
+        marks[2] = 98;
+        marks[3] = 68;
+        marks[4] = 88;
+        System.out.println(marks[0]);
+        System.out.println(marks[1]);
+        System.out.println(marks[2]);
+        System.out.println(marks[3]);
+        System.out.println(marks[4]);
+    }
+}
