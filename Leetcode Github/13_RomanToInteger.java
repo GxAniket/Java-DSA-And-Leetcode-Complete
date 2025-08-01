7efa7bfa6a8d1f6a04fa7dae20f4748325b2f35e
Aniket <sundriyalaniket@gmail.com>
2025-08-01 17:49:24 +0530

    Create 13_RomanToInteger.java

---
+++ Leetcode Github/13_RomanToInteger.java
@@ -0,0 +1,34 @@
+// 13. Roman to Integer leetcode
+import java.util.HashMap;
+import java.util.Map;
+
+public class RomanToInteger {
+    public static void main(String[] args) {
+        String roman = "MCMXCIV"; // Test case
+        int result = romanToInt(roman);
+        System.out.println("Roman: " + roman + " → Integer: " + result);
+    }
+
+    public static int romanToInt(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('I', 1); map.put('V', 5);
+        map.put('X', 10); map.put('L', 50);
+        map.put('C', 100); map.put('D', 500);
+        map.put('M', 1000);
+
+        int result = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            int curr = map.get(s.charAt(i));
+            int next = (i + 1 < s.length()) ? map.get(s.charAt(i + 1)) : 0;
+
+            if (curr < next) {
+                result -= curr;
+            } else {
+                result += curr;
+            }
+        }
+
+        return result;
+    }
+}
