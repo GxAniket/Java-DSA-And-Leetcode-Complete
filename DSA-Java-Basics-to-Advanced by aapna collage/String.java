8f2eb43a4983394634a05dd006b8044fddb4ed4d
Aniket Sundriyal <sundriyalaniket@gmail.com>
2025-08-04 22:39:52 +0530

    Create String.java

---
+++ DSA-Java-Basics-to-Advanced by aapna collage/String.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        String document = "This is an old document. The old content needs to be updated.";
+        String updatedDocument = document.replace("old", "new");
+        System.out.println(updatedDocument);
+    }
+}
