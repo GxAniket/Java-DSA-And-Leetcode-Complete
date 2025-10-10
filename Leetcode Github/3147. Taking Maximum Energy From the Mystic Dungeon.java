f1d33ffab5bb8c23eed6334d753abd7e97fc11ec
Aniket Sundriyal <sundriyalaniket@gmail.com>
2025-10-10 22:13:09 +0530

    Create 3147. Taking Maximum Energy From the Mystic Dungeon.java

---
+++ Leetcode Github/3147. Taking Maximum Energy From the Mystic Dungeon.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maximumEnergy(int[] energy, int k) {
+        int n = energy.length;
+        int maxEnergy = Integer.MIN_VALUE;
+
+        for (int start = 0; start < n; start++) {
+            int sum = 0;
+            for (int i = start; i < n; i += k) {
+                sum += energy[i];
+            }
+            maxEnergy = Math.max(maxEnergy, sum);
+        }
+
+        return maxEnergy;
+    }
+}
