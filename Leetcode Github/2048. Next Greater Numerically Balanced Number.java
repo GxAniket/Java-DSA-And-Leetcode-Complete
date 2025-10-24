487457bbda158425a32177e9f7c9ade1a4fd2165
Aniket Sundriyal <sundriyalaniket@gmail.com>
2025-10-24 21:27:14 +0530

    Create 2048. Next Greater Numerically Balanced Number.java

---
+++ Leetcode Github/2048. Next Greater Numerically Balanced Number.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int nextBeautifulNumber(int n) {
+        int num = n + 1;
+        while (true) {
+            if (isBalanced(num)) return num;
+            num++;
+        }
+    }
+
+    private boolean isBalanced(int num) {
+        int[] count = new int[10];
+        char[] digits = String.valueOf(num).toCharArray();
+
+        for (char c : digits) {
+            count[c - '0']++;
+        }
+
+        for (int i = 0; i < 10; i++) {
+            if (count[i] != 0 && count[i] != i) return false;
+        }
+
+        return true;
+    }
+}
