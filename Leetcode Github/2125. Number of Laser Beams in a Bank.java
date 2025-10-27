b74df17760d38466931885d5535539c7a25867d1
Aniket Sundriyal <sundriyalaniket@gmail.com>
2025-10-27 21:58:07 +0530

    Create 2125. Number of Laser Beams in a Bank.java

---
+++ Leetcode Github/2125. Number of Laser Beams in a Bank.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int numberOfBeams(String[] bank) {
+        int totalBeams = 0;
+        int prevDevices = 0;
+
+        for (String row : bank) {
+            int currentDevices = 0;
+            for (char c : row.toCharArray()) {
+                if (c == '1') currentDevices++;
+            }
+            if (currentDevices > 0) {
+                totalBeams += prevDevices * currentDevices;
+                prevDevices = currentDevices;
+            }
+            // if currentDevices == 0 => skip (prevDevices remains same)
+        }
+
+        return totalBeams;
+    }
+}
