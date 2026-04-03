9a0eeff9b0b230a80b16ab2a15b17fe24d78ffad
Aniket Sundriyal <sundriyalaniket@gmail.com>
2026-04-03 23:01:10 +0530

    Create Tester.java

---
+++ DSA-Java-Basics-to-Advanced by aapna collage/Array basic/Tester.java
@@ -0,0 +1,18 @@
+class Tester {
+	public static void main(String[] args) {
+		
+		double principal1 = 5000;
+		double rate1 = 10;
+		double time1 = 5;
+		
+		double si1 = (principal1 * rate1 * time1) / 100;
+		System.out.println(si1);
+		
+		double principal2 = 3250;
+		double rate2 = 7;
+		double time2 = 3;
+		
+		double si2 = (principal2 * rate2 * time2) / 100;
+		System.out.println(si2);
+	}
+}
