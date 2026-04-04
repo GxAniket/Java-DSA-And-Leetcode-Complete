5689445a28807c3640b877d4be0a86f60a8c8060
Aniket Sundriyal <sundriyalaniket@gmail.com>
2026-04-04 17:44:49 +0530

    Add Customer class for food item ordering
    This program takes a food item order and displays the unit price based on the selection.

---
+++ DSA-Java-Basics-to-Advanced by aapna collage/customer3.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Customer {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter food item (Burger/Pizza/Sandwich): ");
+        String orderedFood = sc.nextLine();
+
+        switch (orderedFood) {
+            case "Burger":
+                System.out.println("You have ordered Burger. Unit price: $10");
+                break;
+
+            case "Pizza":
+                System.out.println("You have ordered Pizza. Unit price: $15");
+                break;
+
+            case "Sandwich":
+                System.out.println("You have ordered Sandwich. Unit price: $8");
+                break;
+
+            default:
+                System.out.println("Invalid selection");
+        }
+
+        sc.close();
+    }
+}
