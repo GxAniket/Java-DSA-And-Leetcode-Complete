fcc13464143ca493ae518b5e7a465d5fd2383852
Aniket Sundriyal <sundriyalaniket@gmail.com>
2026-08-16 23:55:26 +0530

    Create 9 substring slic operations.java

---
+++ DSA Sheet - N/Phase 1 – Array Operations/2) Insert Operations/9 substring slic operations.java
@@ -0,0 +1,9 @@
+public class SubstringOperations {
+    public static void main(String[] args) {
+        String str = "Programming";
+
+        String sub = str.substring(3, 8);
+
+        System.out.println(sub);
+    }
+}
